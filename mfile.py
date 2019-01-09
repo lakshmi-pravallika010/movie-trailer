5c437dca5ceef1c92d748b8c8d1b5bf953e9cb89
pravallika <pravalli010.y@gmail.com>
2019-01-09 16:04:14 +0530

    movie trailer project

---
+++ mfile.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+class cimocylotseivom():
+    def __init__(self,oppopo_egnaro,voiv_otircsa,imder_tacgod,ovonel_tepgip):
+        self.esor=oppopo_egnaro
+        self.nmaahc=voiv_otircsa
+        self.pilut=imder_tacgod
+        self.youtubeurls=ovonel_tepgip
+    def kiujybtg_sreliart(self):
+        webbrowser.open(self.youtubeurls)
---
+++ trailer.py
@@ -0,0 +1,42 @@
+import mfile
+import fresh_tomatoes
+IjNaMuj=mfile.cimocylotseivom( "Bollywood movie ",
+                          " IN LOVE",
+                          "https://i.pinimg.com/originals/f3/b7/04/f3b7048b9a027fc73a884d5af34f80cd.jpg",
+                          "https://youtu.be/TIE92mUvSsw")
+vOmeRigTei=mfile.cimocylotseivom("Tollywood movie",
+                             "COMEDY MOVIE",
+                             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpMQvAKbAOEuKe5kf2NHnNtooa8x2iCobiJkgJXapvKXmCOMDeOA",
+                             "https://youtu.be/3chrcXw1ZQg")
+vOmKeeWenoIe=mfile.cimocylotseivom("Hollywood movie",
+                             "NICE NOVEL",
+                             "https://www.flightnetwork.com/blog/wp-content/uploads/2014/08/one_week_ver3.jpg",
+                             "https://youtu.be/CBgJ0ulNgzk")
+oVmiDnIhIe=mfile.cimocylotseivom("Bollywood movie",
+                                  "REVENGE DRAMA",
+                                  "http://www.cinema.com.my/images/movies/2018/7kedarnath00_450.jpg",
+                                  "https://youtu.be/03-KVRmd3xo")
+UmgUletie=mfile.cimocylotseivom("Tollywood movie",
+                                   "BEAUTIFUL MOVIE",
+                                   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaF3CtHa4l_4zt7TFPenCQUtFHvngCcPQVST7Ghsd7g0jM6iV9",
+                                   "https://youtu.be/PIhS6hti5_4")
+oVmDooWyllOtie=mfile.cimocylotseivom("Tollywood movie",
+                                      "LOVE STORY",
+                                      "https://www.filmibeat.com/img/220x275/popcorn/movie_posters/rangula-ratnam-20180105110641-16784.jpg",
+                                      "https://youtu.be/8wVSSamFj04")
+anayilinujrailla=mfile.cimocylotseivom("Tollywood movie",
+                                       "Entertainment",
+                                       "https://secure-media0.hotstar.com/r1/thumbs/PCTV/39/1000057139/PCTV-1000057139-hcdl.jpg",
+                                       "https://youtu.be/zgXnzAgFsBk")
+vomihtrakulkar=mfile.cimocylotseivom("Tollywood movie",
+                                     "strength of police",
+                                     "https://i.pinimg.com/236x/05/51/10/055110a044c424eceeb2b32a1e981470.jpg",
+                                     "https://youtu.be/n3F9bZN_Vt4")
+theerplukarsavinirs=mfile.cimocylotseivom("Tollywood movie",
+                                          "Love",
+                                          "https://images-na.ssl-images-amazon.com/images/I/61Wf2O5MCrL._SS500.jpg",
+                                          "https://youtu.be/O_gfpsqOEZs")
+
+
+movies=[IjNaMuj,theerplukarsavinirs,vomihtrakulkar,vOmKeeWenoIe,UmgUletie,oVmiDnIhIe,oVmDooWyllOtie,vOmeRigTei,anayilinujrailla]
+fresh_tomatoes.open_movies_page(movies)
